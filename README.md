117e1f3f349e08aa3e4c2287a101be05ab3d98ad
jet-c-21 <67193495+jet-c-21@users.noreply.github.com>
2022-03-23 21:40:53 +0800

    fin #4

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Shell-Script-PlayGround
 The implemented code of the tutorial: https://www.youtube.com/playlist?list=PLS1QulWo1RIYmaxcEqw5JhK3b-6rgdWO_
+## [YouTube Playlist](https://www.youtube.com/playlist?list=PLS1QulWo1RIYmaxcEqw5JhK3b-6rgdWO_)
---
+++ get_user_input.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+#read -p 'username: ' user_val
+#read -sp 'password: ' pwd
+#echo
+#echo "username is $user_val"
+#echo "password is $pwd"
+
+#echo "Enter names: "
+#read -a names
+#echo "Names: ${names[0]} ${names[1]} ${names[2]}"
+
+echo "Enter smth: "
+read
+echo "your input: $REPLY"
